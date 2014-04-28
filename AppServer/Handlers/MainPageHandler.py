6145f1b11b862bbf9c1c2ff00220d7c3031fd480
antonfaint <antonfait17@gmail.com>
2014-04-29 03:13:44 +0400

    add static and urlopen parsers

--- AppServer/Handlers/MainPageHandler.py
+++ AppServer/Handlers/MainPageHandler.py
@@ -1,5 +1,5 @@
 from Handlers.BaseHandler import BaseHandler
-
+import PageParser
 
 class MainPageHandler(BaseHandler):
     def get(self):
--- AppServer/Handlers/PageParser.py
+++ AppServer/Handlers/PageParser.py
@@ -1,5 +1,7 @@
 from contextlib import closing
 from urllib2 import urlopen
+import logging
+from bs4 import BeautifulSoup
 
 
 BASE_URL = "http://goodsmatrix.ru/goods/"
@@ -9,14 +11,68 @@ def getPage(url):
     page = ""
     with closing(urlopen(url)) as url_object:
         page = url_object.read()
-        return page
+    return page
 
 
-def getProductData(product_id):
+def getPageTest():
+    page = ""
     try:
-        url = BASE_URL + str(product_id) + ".html"
-        html = getPage(url)
-        #TODO parse html
+        with open('static/coca_cola.html', 'r') as f:
+            page = f.read()
+    except IOError as e:
+        print "I/O error({0}): {1}".format(e.errno, e.strerror)
 
-    except:
-        print "Exception"
+    return page
+
+#Parse data from html = urlopen(url).read()
+def parseProductData(html):
+    result = []
+
+    soup = BeautifulSoup(html)
+
+    print soup.prettify()
+
+    ingredient_table = soup.find(id='Composition')
+
+    if ingredient_table == None:
+        logging.warning("Empty Result")
+        return result
+
+    for elem in ingredient_table.get_text().split(','):
+        result.append(elem.strip())
+        logging.warning(elem.strip())
+
+    return result
+
+#Parse data from static html file
+def parseProductDataStatic(html):
+    result = []
+
+    soup = BeautifulSoup(html)
+
+    #print soup.prettify()
+
+    ingredient_table = soup.find(id='ctl00_ContentPH_Ingredients_IngrDL')
+
+    if ingredient_table == None:
+        logging.warning("Empty Result")
+        return result
+
+    for elem in ingredient_table.findAll('a'):
+        if elem.parent.name == 'td':
+            result.append(elem.get_text())
+            logging.warning(elem.get_text())
+
+    return result
+
+
+#Main function
+#Returns string list of names of ingredients
+def getProductData(product_id):
+    url = BASE_URL + str(product_id) + ".html"
+    html = getPage(url)
+    if html == "":
+        logging.warning("Empty Result")
+        return []
+
+    return parseProductData(html)
