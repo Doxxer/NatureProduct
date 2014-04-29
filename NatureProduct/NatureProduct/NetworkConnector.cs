788a0f74c141647e62a3fe72ebbfb4f14c70bbfa
iron-phoenix <kluchikhin@gmail.com>
2014-04-29 13:53:19 +0400

    Create synchronous methods

--- NatureProduct/NatureProduct/NetworkConnector.cs
+++ NatureProduct/NatureProduct/NetworkConnector.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace NatProductClientNetwork
@@ -12,29 +13,61 @@ namespace NatProductClientNetwork
     {
         private static readonly string address = "http://nature-product-422.appspot.com/gobo";
 
-        public static void sendRequest(string request)
+        public static string getRequest(string request)
         {
             HttpWebRequest httpWebRequest = (HttpWebRequest)HttpWebRequest.Create(new Uri(address));
             httpWebRequest.Method = "POST";
             httpWebRequest.ContentLength = Encoding.UTF8.GetBytes(request).Length;
             httpWebRequest.ContentType = "text/plain";
-            Stream dataStream = httpWebRequest.GetRequestStream();
+            Stream dataStream = GetRequestStream(httpWebRequest);
             dataStream.Write(Encoding.UTF8.GetBytes(request), 0, Encoding.UTF8.GetBytes(request).Length);
             dataStream.Close();
-            httpWebRequest.BeginGetResponse(new AsyncCallback(ReadCallback), httpWebRequest);
+            HttpWebResponse httpWebResponse = GetResponse(httpWebRequest);
+            using (Stream stream = httpWebResponse.GetResponseStream())
+            {
+                StreamReader reader = new StreamReader(stream, Encoding.UTF8);
+                String responseString = reader.ReadToEnd();
+                return responseString;
+            }
         }
 
-        private static void ReadCallback(IAsyncResult asynchronousResult)
+        public static HttpWebResponse GetResponse(HttpWebRequest request)
         {
-            HttpWebRequest request = (HttpWebRequest)asynchronousResult.AsyncState;
-            HttpWebResponse response = (HttpWebResponse)request.EndGetResponse(asynchronousResult);
-            using (StreamReader streamReader1 = new StreamReader(response.GetResponseStream()))
+            var dataReady = new AutoResetEvent(false);
+            HttpWebResponse response = null;
+            var callback = new AsyncCallback(delegate(IAsyncResult asynchronousResult)
             {
-                string resultString = streamReader1.ReadToEnd();
-                //resultBlock.Text = "Using HttpWebRequest: " + resultString;
-                System.IO.File.WriteAllText(@"C:\Users\Kirill Luchikhin\Documents\test.txt", resultString);
+                response = (HttpWebResponse)request.EndGetResponse(asynchronousResult);
+                dataReady.Set();
+            });
+
+            request.BeginGetResponse(callback, request);
+
+            if (dataReady.WaitOne(10000))
+            {
+                return response;
             }
 
+            return null;
+        }
+
+        public static Stream GetRequestStream(HttpWebRequest request)
+        {
+            var dataReady = new AutoResetEvent(false);
+            Stream stream = null;
+            var callback = new AsyncCallback(delegate(IAsyncResult asynchronousResult)
+            {
+                stream = (Stream)request.EndGetRequestStream(asynchronousResult);
+                dataReady.Set();
+            });
+
+            request.BeginGetRequestStream(callback, request);
+            if (!dataReady.WaitOne(10000))
+            {
+                return null;
+            }
+
+            return stream;
         }
     }
 }
