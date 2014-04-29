ac31c81874459ed2e7b23b6e45c7e0daa80392da
Timur <doxxerACM@gmail.com>
2014-04-29 14:50:05 +0400

    Page parser fix

--- AppServer/PageParser.py
+++ AppServer/PageParser.py
@@ -8,43 +8,42 @@ from bs4 import BeautifulSoup
 BASE_URL = "http://goodsmatrix.ru/goods/"
 
 
-def get_page_content(url):
-    page = ""
-    try:
-        with closing(urlopen(url)) as request:
-            if request.headers.get('content-type'):
-                encoding = request.headers['content-type'].split('charset=')[-1]
-            else:
-                encoding = 'windows-1251'
-            page = unicode(request.read(), encoding)
-    except HTTPError as e:
-        logging.warning("HTTPError: {0} : {1}  ".format(e.errno, e.strerror))
-    except URLError as e:
-        logging.warning("URLError: {0} : {1}  ".format(e.errno, e.strerror))
-
-    return page
-
-
-def parse_product_data(html):
-    result = []
-
-    soup = BeautifulSoup(html)
-
-    ingredient_table = soup.find(id='ctl00_ContentPH_Ingredients_IngrDL')
-
-    if ingredient_table is None:
-        logging.warning("There is no ingredients in " + html)
-        return []
-
-    for elem in ingredient_table.findAll('a'):
-        if elem.parent.name == 'td':
-            caption = elem.get_text().strip()
-            result.append(caption)
-
-    return result
-
-
 def get_product_data(barcode):
+    def get_page_content(url):
+        page = ""
+        try:
+            with closing(urlopen(url)) as request:
+                if request.headers.get('content-type'):
+                    encoding = request.headers['content-type'].split('charset=')[-1]
+                else:
+                    encoding = 'windows-1251'
+                page = unicode(request.read(), encoding)
+        except HTTPError as e:
+            logging.warning("HTTPError: {0} : {1}  ".format(e.errno, e.strerror))
+        except URLError as e:
+            logging.warning("URLError: {0} : {1}  ".format(e.errno, e.strerror))
+
+        return page
+
+
+    def parse_product_data(html):
+        result = []
+
+        soup = BeautifulSoup(html)
+
+        ingredient_table = soup.find(id='ctl00_ContentPH_Ingredients_IngrDL')
+
+        if ingredient_table is None:
+            logging.warning("There is no ingredients in " + html)
+            return []
+
+        for elem in ingredient_table.findAll('a'):
+            if elem.parent.name == 'td':
+                caption = elem.get_text().strip()
+                result.append(caption)
+
+        return result
+
     url = BASE_URL + str(barcode) + ".html"
     html = get_page_content(url)
     if html == "":
