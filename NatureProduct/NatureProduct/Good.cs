ec5ef44f9706051e43e7e57d730ee8dfd5e119f4
iron-phoenix <kluchikhin@gmail.com>
2014-04-29 14:32:17 +0400

    Change format of json request

---
+++ NatureProduct/NatureProduct/Good.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NatProductClientNetwork
+{
+    [DataContract]
+    public class Good
+    {
+        [DataMember]
+        internal string name;
+
+        [DataMember]
+        internal List<EComponent> additives;
+    }
+}
---
+++ NatureProduct/NatureProduct/GoodJson.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.IO;
+
+namespace NatProductClientNetwork
+{
+    class GoodJson
+    {
+        public static string serializeGood(Good good)
+        {
+            MemoryStream mStream = new MemoryStream();
+            DataContractJsonSerializer json = new DataContractJsonSerializer(typeof(Good));
+            json.WriteObject(mStream, good);
+            return Encoding.UTF8.GetString(mStream.ToArray());
+        }
+
+        public static Good deserializeGood(string jsonGood)
+        {
+            DataContractJsonSerializer json = new DataContractJsonSerializer(typeof(Good));
+            return (Good)json.ReadObject(new MemoryStream(Encoding.UTF8.GetBytes(jsonGood)));
+        }
+    }
+}
