442ae2e0aa88386e21df85d4180aa27bbadee758
Timur <doxxerACM@gmail.com>
2014-04-29 12:06:45 +0400

    PageParser tiny fix

--- AppServer/Handlers/PageParser.py
+++ AppServer/Handlers/PageParser.py
@@ -1,6 +1,7 @@
 from contextlib import closing
 from urllib2 import urlopen
 import logging
+
 from bs4 import BeautifulSoup
 
 
@@ -8,7 +9,6 @@ BASE_URL = "http://goodsmatrix.ru/goods/"
 
 
 def getPage(url):
-    page = ""
     with closing(urlopen(url)) as url_object:
         page = url_object.read()
     return page
@@ -24,25 +24,24 @@ def getPageTest():
 
     return page
 
+
 #Parse data from html = urlopen(url).read()
 def parseProductData(html):
     result = []
 
     soup = BeautifulSoup(html)
 
-    print soup.prettify()
-
     ingredient_table = soup.find(id='Composition')
 
-    if ingredient_table == None:
-        logging.warning("Empty Result")
-        return result
+    if ingredient_table is None:
+        logging.warning("There is no ingridients in " + html)
+        return []
 
     for elem in ingredient_table.get_text().split(','):
         result.append(elem.strip())
-        logging.warning(elem.strip())
 
     return result
+
 
 #Parse data from static html file
 def parseProductDataStatic(html):
@@ -50,18 +49,15 @@ def parseProductDataStatic(html):
 
     soup = BeautifulSoup(html)
 
-    #print soup.prettify()
-
     ingredient_table = soup.find(id='ctl00_ContentPH_Ingredients_IngrDL')
 
-    if ingredient_table == None:
-        logging.warning("Empty Result")
-        return result
+    if ingredient_table is None:
+        logging.warning("There is no ingridients in " + html)
+        return []
 
     for elem in ingredient_table.findAll('a'):
         if elem.parent.name == 'td':
             result.append(elem.get_text())
-            logging.warning(elem.get_text())
 
     return result
 
@@ -75,4 +71,9 @@ def getProductData(product_id):
         logging.warning("Empty Result")
         return []
 
-    return parseProductData(html)
+    return parseProductDataStatic(html)
+
+
+if __name__ == "__main__":
+    for ingr in getProductData(5449000000286):
+        print ingr
