ec593943b614f38ecdb619ace6964b51a49e3124
Timur <doxxerACM@gmail.com>
2014-04-29 12:07:09 +0400

    fix

---
+++ AppServer/Handlers/GetProductInfoHandler.py
@@ -0,0 +1,20 @@
+# coding=utf-8
+import json
+
+from Handlers.BaseHandler import BaseHandler
+
+
+class GetProductInfoHandler(BaseHandler):
+    def getAdditives(self):
+        result = []
+        for i in range(5):
+            ad = dict(id=str(100 + i), category=u"Категория", names=[u"Имя1", u"Имя2", u"Имя3"], severity=i,
+                      comment=u"Вызывает слонёнка Гобо")
+            result.append(ad)
+        return result
+
+
+    def post(self):
+        response = {'name': u"Тестовое имя товара", 'additives': self.getAdditives()}
+        self.response.set_status(200)
+        self.response.write(json.dumps(response, ensure_ascii=False))
