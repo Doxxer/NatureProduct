1fd89ef5c38c42bc83b28c98851b21fe3478f246
Timur <doxxerACM@gmail.com>
2014-04-29 17:01:14 +0400

    AppServer fix

---
+++ AppServer/Handlers/GetProductInfoHandler.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+import json
+
+from GetAdditiveInfo import get_additive
+
+from Handlers.BaseHandler import BaseHandler
+from PageParser import get_product_data
+
+
+class GetProductInfoHandler(BaseHandler):
+    def getAdditives(self, ingredients):
+        result = []
+        for ingredient in ingredients:
+            additive = get_additive(ingredient)
+            if additive:
+                result.append(additive)
+        return result
+
+    def post(self):
+        barcode = self.request.body
+        if barcode.find('=') == -1:
+            self.response.set_status(500)
+
+        barcode = barcode.split('=')[1].strip()
+
+        name, ingredients = get_product_data(barcode)
+
+        response = {'name': name, 'additives': self.getAdditives(ingredients)}
+        self.response.set_status(200)
+        self.response.write(json.dumps(response, ensure_ascii=False))
