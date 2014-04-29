fa9c8e3e95e0f338be5ee3b0eafdc07b232c6e1c
antonfaint <antonfait17@gmail.com>
2014-04-29 12:44:20 +0400

    PageParser Fix

--- AppServer/Handlers/PageParser.py
+++ AppServer/Handlers/PageParser.py
@@ -1,5 +1,5 @@
 from contextlib import closing
-from urllib2 import urlopen
+from urllib2 import urlopen, HTTPError, URLError
 import logging
 
 from bs4 import BeautifulSoup
@@ -9,10 +9,16 @@ BASE_URL = "http://goodsmatrix.ru/goods/"
 
 
 def getPage(url):
-    with closing(urlopen(url)) as url_object:
-        page = url_object.read()
-    return page
+    page = ""
+    try:
+        with closing(urlopen(url)) as url_object:
+            page = url_object.read()
+    except HTTPError as e:
+        logging.warning("HTTPError: {0} : {1}  ".format(e.errno, e.strerror) )
+    except URLError as e:
+        logging.warning("URLError: {0} : {1}  ".format(e.errno, e.strerror) )
 
+    return page
 
 def getPageTest():
     page = ""
@@ -34,7 +40,7 @@ def parseProductData(html):
     ingredient_table = soup.find(id='Composition')
 
     if ingredient_table is None:
-        logging.warning("There is no ingridients in " + html)
+        logging.warning("There is no ingredients in " + html)
         return []
 
     for elem in ingredient_table.get_text().split(','):
@@ -57,7 +63,7 @@ def parseProductDataStatic(html):
 
     for elem in ingredient_table.findAll('a'):
         if elem.parent.name == 'td':
-            result.append(elem.get_text())
+            result.append(elem.get_text().strip())
 
     return result
 
@@ -75,5 +81,5 @@ def getProductData(product_id):
 
 
 if __name__ == "__main__":
-    for ingr in getProductData(5449000000286):
+    for ingr in getProductData(4810206001901):
         print ingr
